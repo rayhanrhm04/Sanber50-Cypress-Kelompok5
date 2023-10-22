45599fcedef1c61ca65e9bfdd98652f81c6191c5
Rizki Hernawan Wicaksono <rizkihernawan2121@gmail.com>
2023-10-22 11:55:35 +0700

    Update view and update shopping cart

--- cypress/e2e/viewAndupdateCart.cy.js
+++ cypress/e2e/viewAndupdateCart.cy.js
@@ -6,14 +6,12 @@ describe('View and Update Shopping Cart', () => {
       cy.get('#pass').type('No123456');
       cy.get('#send2').click();
 
-      cy.wait(1500);
+      cy.reload();
       cy.get('a.action.showcart').click();
-      cy.wait(1500);
-      cy.get('a.action.showcart').click();
-      cy.wait(2500);
-      cy.get('a.action.viewcart').click();
-      cy.wait(1500);
-      cy.get('input#cart-399134-qty.input-text.qty').clear().type('2');
+      // cy.reload();
+      // cy.get('a.action.showcart').click();
+      // cy.get('a.action.viewcart').click();
+      cy.get('input#cart-399174-qty.input-text.qty').clear().type('2');
 
       cy.get('button.action.update').click();
     });
