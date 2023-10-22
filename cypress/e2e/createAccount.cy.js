6b9a03235b6962b96f2908009cc2cb1edcf23ae4
rayhan <haimac@names-MacBook-Pro.local>
2023-10-22 10:36:59 -0700

    create account

--- cypress/e2e/createAccount.cy.js
+++ cypress/e2e/createAccount.cy.js
@@ -1,5 +1,16 @@
 describe('Create an Account', ()=> {
-    it('Successfully Login', ()=> {
-        cy.visit('/customer/account/create/')
-    })
-})
+    it('Visit Website Page', ()=> {
+        //masuk ke dalam website
+        cy.visit('/customer/account/create/');
+        cy.title('eq', 'Create New Customer Account');
+        // Cypress.on('uncaught:exception', () => { return false })
+        //input form register
+        cy.get('input[name="firstname"]').type('Ray');
+        cy.get('input[name="lastname"]').type('Rahman');
+        cy.get('input[name="email"]').type('rayhan1234@gmail.com');
+        cy.get('input[name="password"]').type('NoPassword01');
+        cy.get('input[name="password_confirmation"]').type('NoPassword01');
+        //cek button create an account
+        cy.get('button.action.submit').click();
+    });
+});
