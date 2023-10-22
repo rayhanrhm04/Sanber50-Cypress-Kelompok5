d45fe960efe4850e40f0e9c9c118725adeee117b
Rizki Hernawan Wicaksono <rizkihernawan2121@gmail.com>
2023-10-22 12:02:23 +0700

    Menambahkan comment pada steps view and update cart

--- cypress/e2e/viewAndupdateCart.cy.js
+++ cypress/e2e/viewAndupdateCart.cy.js
@@ -1,18 +1,23 @@
 describe('View and Update Shopping Cart', () => {
     it('View and Update Shopping Cart', () => {
+      //Login with valid account
       cy.visit('/customer/account/login');
 
       cy.get('#email').type('rizkikiriz@gmail.com');
       cy.get('#pass').type('No123456');
       cy.get('#send2').click();
 
+      //Reload the website 
       cy.reload();
+
+      //Click shopping cart
       cy.get('a.action.showcart').click();
       // cy.reload();
       // cy.get('a.action.showcart').click();
       // cy.get('a.action.viewcart').click();
-      cy.get('input#cart-399174-qty.input-text.qty').clear().type('2');
 
+      //Change qty cart and update
+      cy.get('input#cart-399174-qty.input-text.qty').clear().type('2');
       cy.get('button.action.update').click();
     });
   });
