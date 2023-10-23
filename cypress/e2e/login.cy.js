7e2fb8addc7746040832db9d97b39c5d4c860bc7
Lilyan <lilyan0800@gmail.com>
2023-10-23 16:39:58 +0700

    update login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,38 +1,44 @@
+import loginPage from "../support/loginPage";
+
 describe("Customer Login", () => {
+  beforeEach(() => {
+    cy.visit("");
+  });
   it("Success Login Magento", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
+    // cy.visit("https://magento.softwaretestingboard.com/");
 
     cy.contains("Sign In").click();
-    cy.get("#email").type("lilyan@gmail.com");
-    cy.get("#pass").type("Lilyan123");
-    cy.get("#send2").contains("Sign In").click();
+    // cy.get("#email").type("lilyan@gmail.com");
+    cy.get(loginPage.email).type("lilyan@gmail.com");
+    cy.get(loginPage.passw).type("Lilyan123");
+    cy.get(loginPage.loginBtn).contains("Sign In").click();
   });
 
   it("Failed Login with invalid email", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
+    // cy.visit("https://magento.softwaretestingboard.com/");
 
     cy.contains("Sign In").click();
-    cy.get("#email").type("lilyan08@gmail.com");
-    cy.get("#pass").type("Lilyan123");
-    cy.get("#send2").contains("Sign In").click();
-    cy.get(".message-error").should("be.visible");
+    cy.get(loginPage.email).type("lilyan08@gmail.com");
+    cy.get(loginPage.passw).type("Lilyan123");
+    cy.get(loginPage.loginBtn).contains("Sign In").click();
+    cy.get(loginPage.error_msg).should("be.visible");
   });
 
   it("Failed Login with invalid password", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
+    // cy.visit("https://magento.softwaretestingboard.com/");
 
     cy.contains("Sign In").click();
-    cy.get("#email").type("lilyan08@gmail.com");
-    cy.get("#pass").type("Lilyan321");
-    cy.get("#send2").contains("Sign In").click();
-    cy.get(".message-error").should("be.visible");
+    cy.get(loginPage.email).type("lilyan08@gmail.com");
+    cy.get(loginPage.passw).type("Lilyan321");
+    cy.get(loginPage.loginBtn).contains("Sign In").click();
+    cy.get(loginPage.error_msg).should("be.visible");
   });
 
   it("Failed Login with empty email & password", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
+    // cy.visit("https://magento.softwaretestingboard.com/");
 
     cy.contains("Sign In").click();
-    cy.get("#send2").contains("Sign In").click();
-    cy.get(".message-error").should("be.visible");
+    cy.get(loginPage.loginBtn).contains("Sign In").click();
+    cy.get(loginPage.error_msg).should("be.visible");
   });
 });
---
+++ cypress/support/loginPage.js
@@ -0,0 +1,7 @@
+class loginPage {
+  email = "#email";
+  passw = "#pass";
+  loginBtn = "#send2";
+  error_msg = ".message-error";
+}
+export default new loginPage();
